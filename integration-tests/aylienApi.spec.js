4b95a5ca9480f687c84eddecd4113a0b5d036801
Alex Seiden <alex.seiden@gmail.com>
2017-04-27 15:39:20 -0700

    (bug) implement try/catch block to manage lack of api keys in certain envs

--- integration-tests/aylienApi.spec.js
+++ integration-tests/aylienApi.spec.js
@@ -1,17 +1,21 @@
 const chai = require('chai');
 const AylienNewsApi = require('aylien-news-api');
 
-const getNews = require('../utilities/aylienApi.js');
-const apiInfo = require('../lib/env/aylienApiKeys');
+const getNews = require('../utilities/aylienApi');
+try {
+  const apiInfo = require('../lib/env/aylienApiKeys');
 
-const expect = chai.expect;
-const apiInstance = new AylienNewsApi.DefaultApi();
+  const expect = chai.expect;
+  const apiInstance = new AylienNewsApi.DefaultApi();
 
-let appId = apiInstance.apiClient.authentications['app_id'];
-appId.apiKey = apiInfo.id;
+  let appId = apiInstance.apiClient.authentications['app_id'];
+  appId.apiKey = apiInfo.id;
 
-let appKey = apiInstance.apiClient.authentications['app_key'];
-appKey.apiKey = apiInfo.key;
+  let appKey = apiInstance.apiClient.authentications['app_key'];
+  appKey.apiKey = apiInfo.key;
+} catch (e) {
+  console.log(e);
+}
 
 xdescribe('Aylien API', () => {
 
