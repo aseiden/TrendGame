d9efc2a4f9a704ca7e46f45cbd434fe24223c950
Alex Seiden <alex.seiden@gmail.com>
2017-04-27 10:16:26 -0700

    (feat) test peakAlgo file

--- utilities/peakAlgo.js
+++ utilities/peakAlgo.js
@@ -3,9 +3,12 @@ const findPeaks = function(timeSeries) {
 
   timeSeries.forEach((data) => {
     if (data.value > peaks[0][1]) {
+      peaks.length = 0;
       peaks.push([data.time, data.value]);
     }
   });
 
   return peaks;
 };
+
+module.exports = findPeaks;
