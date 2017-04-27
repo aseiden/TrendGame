7a47d7bc3dcdfa9b684c04cbedd487f128d866c5
Alex Seiden <alex.seiden@gmail.com>
2017-04-27 11:26:00 -0700

    (feat) add aylient npm module

--- integration-tests/aylienApi.spec.js
+++ integration-tests/aylienApi.spec.js
@@ -1,8 +1,12 @@
 var chai = require('chai');
+var AylienNewsApi = require('aylien-news-api');
 
 var getNews = require('../utilities/aylienApi.js');
 
 var expect = chai.expect;
+var apiInstance = new AylienNewsApi.DefaultApi();
+var appKey = apiInstance.apiClient.authentications['app_key'];
+appKey.apiKey = '';
 
 describe('Aylien API', () => {
 
